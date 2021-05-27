c7a339879a0fe9162f47ef7512b9509144bfc9d0
LuongLegend <luongprolv99@gmail.com>
2021-05-28 04:12:55 +0700

    change api host

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const API_HOST = 'http://localhost:3326'
+export const API_HOST = 'https://llbook-api.herokuapp.com'
 export const TINY_API_KEY = 'mbc05lhlp8rxoebbv0klufd7auraatoeyzpv7c820dsu8wy5'
 export const RPP = 10 //row per page
 export const DATE_FORMAT = 'DD/MM/YYYY'
